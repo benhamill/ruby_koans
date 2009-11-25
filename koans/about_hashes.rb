2174cf188d7884383ac8fd874d1a8716174a4351
Ben Hamill <git-commits@benhamill.com>
2009-11-25 09:24:49 -0600

    Arrays, control, exceptions, hashes, iterators, methods, strings, and true/false.

--- koans/about_hashes.rb
+++ koans/about_hashes.rb
@@ -5,26 +5,26 @@ class AboutHashes < EdgeCase::Koan
     empty_hash = Hash.new
     assert_equal Hash, empty_hash.class
     assert_equal({}, empty_hash)
-    assert_equal __, empty_hash.size
+    assert_equal 0, empty_hash.size
   end
 
   def test_hash_literals
     hash = { :one => "uno", :two => "dos" }
-    assert_equal __, hash.size
+    assert_equal 2, hash.size
   end
 
   def test_accessing_hashes
     hash = { :one => "uno", :two => "dos" }
-    assert_equal __, hash[:one]
-    assert_equal __, hash[:two]
-    assert_equal __, hash[:doesnt_exist]
+    assert_equal 'uno', hash[:one]
+    assert_equal 'dos', hash[:two]
+    assert_equal nil, hash[:doesnt_exist]
   end
 
   def test_changing_hashes
     hash = { :one => "uno", :two => "dos" }
     hash[:one] = "eins"
 
-    expected = { :one => __, :two => "dos" }
+    expected = { :one => 'eins', :two => "dos" }
     assert_equal expected, hash
 
     # Bonus Question: Why was "expected" broken out into a variable
@@ -41,8 +41,8 @@ class AboutHashes < EdgeCase::Koan
   def test_hash_keys_and_values
     hash = { :one => "uno", :two => "dos" }
 
-    assert_equal __, hash.keys.sort
-    assert_equal __, hash.values.sort
+    assert_equal [:one, :two], hash.keys.sort
+    assert_equal ['dos', 'uno'], hash.values.sort
   end
 
   def test_combining_hashes
@@ -51,7 +51,7 @@ class AboutHashes < EdgeCase::Koan
 
     assert_not_equal hash, new_hash
     
-    expected = { "jim" => __, "amy" => 20, "dan" => 23, "jenny" => __ }
+    expected = { "jim" => 54, "amy" => 20, "dan" => 23, "jenny" => 26 }
     assert_equal expected, new_hash
   end
 end
--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,19 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if a + b == c or
+     b + c == a or
+     c + a == b
+    raise TriangleError, 'Sum of two sides equal to third.'
+  end
+  
+  raise(TriangleError, 'Not all sides are greater than 0.') if not (a > 0 and b > 0 and c > 0)
+
+  return :equilateral if a == b and b == c
+
+  return :isosceles if a == b or b == c or c == a
+
+  return :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
